62bcb9f8891321c9b72f0bbb960a20efe5f2a305
jared-g <jared-g@live.ca>
2018-05-16 22:06:50 -0400

    Feature parity with old version using Vue
    -Refactored site using Vue.js
    -Added lodash
    -Added new pets

--- files/js/data.js
+++ files/js/data.js
@@ -1,108 +1,231 @@
-const data = {
+/* global Vue, _, Sortable */
+Vue.directive('sortable', {
+  inserted(el, binding) {
+    const sortable = new Sortable(el, binding.value || {});
+  },
+});
+
+const originalData = {
+  version: '2.1.1',
   petList: [
-    { name: 'Mambo', fragments: 180, img: 1, farm: true, reqs: [2, 3, 13, 104] },
-    { name: 'Joe', fragments: 10, img: 2, farm: false, reqs: [4, 5, 17, 106] },
-    { name: 'Mummy', fragments: 10, img: 3, farm: false, reqs: [6, 7, 21, 108] },
-    { name: 'Piggy', fragments: 10, img: 4, farm: true, reqs: [8, 9, 25, 110] },
-    { name: 'Potang', fragments: 10, img: 5, farm: false, reqs: [10, 11, 29, 112] },
-    { name: 'Uni', fragments: 10, img: 14, farm: false, reqs: [12, 15, 33, 114] },
-    { name: 'Joker', fragments: 10, img: 7, farm: false, reqs: [14, 19, 37, 116] },
-    { name: 'Snork', fragments: 10, img: 8, farm: true, reqs: [16, 23, 41, 118] },
-    { name: 'Phoenic', fragments: 10, img: 9, farm: true, reqs: [18, 27, 45, 120] },
-    { name: 'Indy', fragments: 10, img: 10, farm: true, reqs: [20, 31, 49, 122] },
-    { name: 'Grim', fragments: 10, img: 11, farm: false, reqs: [22, 35, 53, 124] },
-    { name: 'Woola', fragments: 10, img: 12, farm: false, reqs: [24, 39, 57, 126] },
-    { name: 'Lamp', fragments: 10, img: 13, farm: true, reqs: [26, 43, 61, 128] },
-    { name: 'Tinkey', fragments: 80, img: 6, farm: true, reqs: [28, 47, 65, 130] },
-    { name: 'Sleepy', fragments: 10, img: 15, farm: true, reqs: [30, 51, 69, 132] },
-    { name: 'Totem', fragments: 10, img: 16, farm: true, reqs: [32, 55, 73, 134] },
-    { name: 'Icy', fragments: 50, img: 17, farm: true, reqs: [34, 59, 77, 136] },
-    { name: 'Squirrel', fragments: 10, img: 18, farm: true, reqs: [36, 63, 81, 138] },
-    { name: 'Moon', fragments: 30, img: 19, farm: true, reqs: [38, 67, 85, 140] },
-    { name: 'Boom', fragments: 10, img: 28, farm: true, reqs: [40, 71, 89, 142] },
-    { name: 'Leo', fragments: 10, img: 26, farm: false, reqs: [42, 75, 93, 144] },
-    { name: 'Devil', fragments: 10, img: 27, farm: true, reqs: [44, 79, 97, 146] },
-    { name: 'Griffy', fragments: 10, img: 29, farm: false, reqs: [46, 83, 101, 148] },
-    { name: 'Mir', fragments: 10, img: 36, farm: true, reqs: [48, 87, 105, 150] },
-    { name: 'Nimbus', fragments: 0, img: 37, farm: true, reqs: [50, 91, 109, 152] },
-    { name: 'Jingger', fragments: 0, img: 38, farm: true, reqs: [52, 95, 113, 154] },
-    { name: 'Rapty', fragments: 0, img: 39, farm: true, reqs: [54, 99, 117, 156] },
-    { name: 'Winky', fragments: 0, img: 40, farm: true, reqs: [56, 103, 121, 158] },
-    { name: 'Bunny', fragments: 0, img: 41, farm: true, reqs: [58, 107, 125, 160] },
-    { name: 'Cora', fragments: 0, img: 42, farm: true, reqs: [60, 111, 129, 162] },
-    { name: 'Wakong', fragments: 0, img: 43, farm: true, reqs: [62, 115, 133, 164] },
-    { name: 'E-77', fragments: 0, img: 48, farm: true, reqs: [64, 119, 137, 166] },
-    { name: 'Windy', fragments: 0, img: 49, farm: true, reqs: [66, 123, 141, 168] },
-    { name: 'Dark-Snake', fragments: 0, img: 50, farm: true, reqs: [68, 127, 145, 170] },
-    { name: 'Seahorse', fragments: 0, img: 51, farm: true, reqs: [70, 131, 149, 172] },
-    { name: 'Sarah', fragments: 0, img: 55, farm: true, reqs: [72, 135, 153, 174] },
-    { name: 'Oscar', fragments: 0, img: 56, farm: true, reqs: [74, 139, 157, 176] },
-    { name: 'Black', fragments: 0, img: 57, farm: true, reqs: [76, 143, 161, 178] },
-    { name: 'Juda', fragments: 0, img: 58, farm: true, reqs: [78, 147, 165, 180] },
-    { name: 'Rena', fragments: 0, img: 93, farm: true, reqs: [80, 151, 169, 182] },
-    { name: 'Hippong', fragments: 0, img: 94, farm: true, reqs: [82, 155, 173, 184] },
-    { name: 'Osma', fragments: 0, img: 95, farm: true, reqs: [84, 159, 177, 186] },
-    { name: 'Saul', fragments: 0, img: 96, farm: true, reqs: [86, 163, 181, 188] },
-    { name: 'Bernard', fragments: 0, img: 112, farm: true, reqs: [88, 167, 185, 190] },
-    { name: 'ChiChi', fragments: 0, img: 113, farm: true, reqs: [90, 171, 189, 192] },
-    { name: 'Chesher', fragments: 0, img: 114, farm: true, reqs: [92, 175, 193, 194] },
-    { name: 'Bamba', fragments: 0, img: 115, farm: true, reqs: [94, 179, 197, 196] },
-    { name: 'Cat-Knight', fragments: 0, img: 136, farm: true, reqs: [96, 183] },
-    { name: 'Mori', fragments: 0, img: 137, farm: true, reqs: [98, 187] },
-    { name: 'Cerbero', fragments: 0, img: 138, farm: true, reqs: [100, 191] },
-    { name: 'Octopa', fragments: 0, img: 139, farm: true, reqs: [102, 195] },
+    { name: 'Hippong', fragments: 0, img: 94, farm: true, index: 40, defaultTier: 1 },
+    { name: 'Tinkey', fragments: 10, img: 6, farm: true, index: 13, defaultTier: 2 },
+    { name: 'Icy', fragments: 10, img: 17, farm: true, index: 16, defaultTier: 3 },
+    { name: 'Cat Knight', fragments: 0, img: 136, farm: true, index: 47, defaultTier: 4 },
+    { name: 'Boom', fragments: 10, img: 28, farm: true, index: 19, defaultTier: 5 },
+    { name: 'Wakong', fragments: 0, img: 43, farm: true, index: 30, defaultTier: 6 },
+    { name: 'Octopa', fragments: 0, img: 139, farm: true, index: 50, defaultTier: 7 },
+    { name: 'Bamba', fragments: 0, img: 115, farm: true, index: 46, defaultTier: 8 },
+    { name: 'Seahorse', fragments: 0, img: 51, farm: true, index: 34, defaultTier: 9 },
+    { name: 'Moon', fragments: 10, img: 19, farm: true, index: 18, defaultTier: 10 },
+    { name: 'Sarah', fragments: 0, img: 55, farm: true, index: 35, defaultTier: 11 },
+    { name: 'E-77', fragments: 0, img: 48, farm: true, index: 31, defaultTier: 12 },
+    { name: 'Dark Snake', fragments: 0, img: 50, farm: true, index: 33, defaultTier: 13 },
+    { name: 'Windy', fragments: 0, img: 49, farm: true, index: 32, defaultTier: 14 },
+    { name: 'Cerbero', fragments: 0, img: 138, farm: true, index: 49, defaultTier: 15 },
+    { name: 'Athos', fragments: 0, img: 201, farm: true, index: 51, defaultTier: 16 },
+    { name: 'Grizzle', fragments: 0, img: 202, farm: true, index: 52, defaultTier: 17 },
+    { name: 'Raven', fragments: 0, img: 203, farm: true, index: 53, defaultTier: 18 },
+    { name: 'Leyak', fragments: 0, img: 204, farm: true, index: 54, defaultTier: 19 },
+    { name: 'Osma', fragments: 0, img: 95, farm: true, index: 41, defaultTier: 20 },
+    { name: 'Saul', fragments: 0, img: 96, farm: true, index: 42, defaultTier: 21 },
+    { name: 'Rena', fragments: 0, img: 93, farm: true, index: 39, defaultTier: 22 },
+    { name: 'Bernard', fragments: 0, img: 112, farm: true, index: 43, defaultTier: 23 },
+    { name: 'ChiChi', fragments: 0, img: 113, farm: true, index: 44, defaultTier: 24 },
+    { name: 'Mori', fragments: 0, img: 137, farm: true, index: 48, defaultTier: 25 },
+    { name: 'Mambo', fragments: 10, img: 1, farm: true, index: 0, defaultTier: 26 },
+    { name: 'Totem', fragments: 10, img: 16, farm: true, index: 15, defaultTier: 27 },
+    { name: 'Lamp', fragments: 10, img: 13, farm: true, index: 12, defaultTier: 28 },
+    { name: 'Sleepy', fragments: 10, img: 15, farm: true, index: 14, defaultTier: 29 },
+    { name: 'Jingger', fragments: 0, img: 38, farm: true, index: 25, defaultTier: 30 },
+    { name: 'Oscar', fragments: 0, img: 56, farm: true, index: 36, defaultTier: 31 },
+    { name: 'Nimbus', fragments: 0, img: 37, farm: true, index: 24, defaultTier: 32 },
+    { name: 'Mir', fragments: 10, img: 36, farm: true, index: 23, defaultTier: 33 },
+    { name: 'Squirrel', fragments: 10, img: 18, farm: true, index: 17, defaultTier: 34 },
+    { name: 'Phoenic', fragments: 10, img: 9, farm: true, index: 8, defaultTier: 35 },
+    { name: 'Devil', fragments: 10, img: 27, farm: true, index: 21, defaultTier: 36 },
+    { name: 'Chesher', fragments: 0, img: 114, farm: true, index: 45, defaultTier: 37 },
+    { name: 'Winky', fragments: 0, img: 40, farm: true, index: 27, defaultTier: 38 },
+    { name: 'Cora', fragments: 0, img: 42, farm: true, index: 29, defaultTier: 39 },
+    { name: 'Bunny', fragments: 0, img: 41, farm: true, index: 28, defaultTier: 40 },
+    { name: 'Indy', fragments: 10, img: 10, farm: true, index: 9, defaultTier: 41 },
+    { name: 'Snork', fragments: 10, img: 8, farm: true, index: 7, defaultTier: 42 },
+    { name: 'Piggy', fragments: 10, img: 4, farm: true, index: 3, defaultTier: 43 },
+    { name: 'Rapty', fragments: 0, img: 39, farm: true, index: 26, defaultTier: 44 },
+    { name: 'Black', fragments: 0, img: 57, farm: true, index: 37, defaultTier: 45 },
+    { name: 'Juda', fragments: 0, img: 58, farm: true, index: 38, defaultTier: 46 },
+    { name: 'Joe', fragments: 10, img: 2, farm: false, index: 1, defaultTier: 47 },
+    { name: 'Mummy', fragments: 10, img: 3, farm: false, index: 2, defaultTier: 48 },
+    { name: 'Potang', fragments: 10, img: 5, farm: false, index: 4, defaultTier: 49 },
+    { name: 'Uni', fragments: 10, img: 14, farm: false, index: 5, defaultTier: 50 },
+    { name: 'Joker', fragments: 10, img: 7, farm: false, index: 6, defaultTier: 51 },
+    { name: 'Grim', fragments: 10, img: 11, farm: false, index: 10, defaultTier: 52 },
+    { name: 'Woola', fragments: 10, img: 12, farm: false, index: 11, defaultTier: 53 },
+    { name: 'Leo', fragments: 10, img: 26, farm: false, index: 20, defaultTier: 54 },
+    { name: 'Griffy', fragments: 10, img: 29, farm: false, index: 22, defaultTier: 55 },
   ],
-  tierList: [
-    'Tinkey',
-    'Icy',
-    'Cat-Knight',
-    'Hippong',
-    'Boom',
-    'Wakong',
-    'Octopa',
-    'Bamba',
-    'Seahorse',
-    'Moon',
-    'Sarah',
-    'E-77',
-    'Dark-Snake',
-    'Windy',
-    'Cerbero',
-    'Osma',
-    'Saul',
-    'Rena',
-    'Bernard',
-    'ChiChi',
-    'Mori',
-    'Mambo',
-    'Totem',
-    'Lamp',
-    'Sleepy',
-    'Jingger',
-    'Oscar',
-    'Nimbus',
-    'Mir',
-    'Squirrel',
-    'Phoenic',
-    'Devil',
-    'Chesher',
-    'Winky',
-    'Cora',
-    'Bunny',
-    'Indy',
-    'Snork',
-    'Piggy',
-    'Rapty',
-    'Black',
-    'Juda',
-    'Joe',
-    'Mummy',
-    'Potang',
-    'Uni',
-    'Joker',
-    'Grim',
-    'Woola',
-    'Leo',
-    'Griffy',
-  ],
+  SHList: [],
+  KL: 100,
+  entries: 10,
+  refills: 3,
+  gemCostArray: [0, 100, 300, 700, 1500, 2700, 4300],
+  farmList: [],
 };
+
+// Load saved data if it exists
+const savedData = JSON.parse(localStorage.getItem('data')) || false;
+let data;
+if (savedData) {
+  if (savedData.version === originalData.version) {
+    data = _.cloneDeep(savedData);
+  } else {
+    // Do something if data versions are different
+  }
+} else {
+  data = _.cloneDeep(originalData);
+  // Migrate very old data if it exists
+  const oldPetList = JSON.parse(localStorage.getItem('petList')) || false;
+  if (oldPetList) {
+    localStorage.setItem('petList', false);
+    for (let i = 0; i < oldPetList.petList.length; i++) {
+      for (let j = 0; j < data.petList.length; j++) {
+        if (oldPetList.name === data.petList[j].name.replace(' ', '-')) {
+          data.petList[j].fragments = oldPetList.fragments;
+          data.petList[j].farm = oldPetList.farm;
+        }
+      }
+    }
+  }
+}
+
+const orderedPetList = _.orderBy(data.petList, 'index');
+for (let i = 0; i < orderedPetList.length * 2 + 10; i++) {
+  data.SHList[i] = [{}, {}];
+}
+
+// Create SH pages
+for (let i = 0; i < orderedPetList.length; i++) {
+  data.SHList[i][0].img = orderedPetList[i].img;
+  data.SHList[i][0].name = orderedPetList[i].name;
+  data.SHList[i + orderedPetList.length][0].img = orderedPetList[i].img;
+  data.SHList[i + orderedPetList.length][0].name = orderedPetList[i].name;
+  if (i < 5) {
+    data.SHList[i][1].img = orderedPetList[i].img;
+    data.SHList[i][1].name = orderedPetList[i].name;
+  } else {
+    data.SHList[(i - 2) * 2][1].img = orderedPetList[i].img;
+    data.SHList[(i - 2) * 2][1].name = orderedPetList[i].name;
+  }
+  data.SHList[i * 2 + 5][1].img = orderedPetList[i].img;
+  data.SHList[i * 2 + 5][1].name = orderedPetList[i].name;
+}
+
+// Create SH page numbers
+for (let i = 0, page = 1, pageIndex = 1; i < data.SHList.length; i++) {
+  data.SHList[i][0].KLReq = `${i * 2 + 2}`;
+  data.SHList[i][0].stage = `${page}-${pageIndex}`;
+  pageIndex += 4;
+  data.SHList[i][1].stage = `${page}-${pageIndex}`;
+  pageIndex++;
+  if (pageIndex > 20) {
+    pageIndex = 1;
+    page++;
+  }
+}
+
+// Trim off excess stages
+data.SHList.length = data.petList.length * 2 - 4;
+
+const vm = new Vue({
+  el: '#app',
+  data,
+  created() {
+    this.updateFarmList();
+  },
+  watch: {
+    petList: {
+      handler(val) {
+        this.updateFarmList();
+      },
+    },
+    KL() {
+      this.updateFarmList();
+    },
+    entries() {
+      this.updateFarmList();
+    },
+    refills() {
+      this.updateFarmList();
+    },
+  },
+  computed: {
+    orderedPetList() {
+      return _.orderBy(data.petList, 'index');
+    },
+  },
+  methods: {
+    getPet(petName) {
+      let pet = data.petList[0];
+      for (let i = 1; i < data.petList.length; i++) {
+        if (data.petList[i].name === petName) {
+          pet = data.petList[i];
+        }
+      }
+      return pet;
+    },
+    reorderPetList({ oldIndex, newIndex }) {
+      const movedItem = data.petList.splice(oldIndex, 1)[0];
+      data.petList.splice(newIndex, 0, movedItem);
+    },
+    resetTierList() {
+      data.petList = _.orderBy(data.petList, 'defaultTier');
+    },
+    addFarmedFrags() {
+      for (const pet of data.petList) {
+        pet.fragments += pet.farmableFrags;
+      }
+      this.updateFarmList();
+    },
+    updateFarmList() {
+      // Reset farmableFrags to zero on all pets
+      _.forEach(data.petList, (obj) => {
+        _.set(obj, 'farmableFrags', 0);
+      });
+
+      // Save data
+      localStorage.setItem('data', JSON.stringify(data));
+
+      data.farmList = _.filter(data.petList, obj => obj.fragments < 330 && obj.farm);
+      let tickets = data.entries + data.refills * 5;
+
+      for (const pet of data.farmList) {
+        const availableFrags = _.filter(data.SHList, obj => obj[0].KLReq <= data.KL && (obj[0].name === pet.name || obj[1].name === pet.name));
+        pet.farmableFrags = 0;
+        if (availableFrags !== []) {
+          let firstStageFrags = 3;
+          for (let i = 0; i < availableFrags.length; i++) {
+            if (availableFrags[i][0].name === pet.name) {
+              pet.farmableFrags += firstStageFrags;
+              // Only the first first stage has 3 frags
+              if (firstStageFrags === 3) {
+                firstStageFrags = 1;
+              }
+            }
+            if (availableFrags[i][1].name === pet.name) {
+              pet.farmableFrags += 3;
+            }
+          }
+        }
+        // If we're farming more frags than needed to finish the pet, remove the excess, or more than we have remaining tickets, remove excess
+        pet.farmableFrags = pet.farmableFrags > 330 - pet.fragments ? 330 - pet.fragments : pet.farmableFrags;
+        pet.farmableFrags = pet.farmableFrags > tickets ? tickets : pet.farmableFrags;
+        pet.daysRemaining = Math.ceil((330 - pet.fragments) / pet.farmableFrags);
+        tickets -= pet.farmableFrags;
+        if (tickets <= 0) {
+          break;
+        }
+      }
+      data.farmList = _.filter(data.farmList, obj => obj.farmableFrags > 0);
+    },
+  },
+});
